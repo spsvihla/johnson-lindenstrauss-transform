8970636000f02c6682d4e97e7550488a77b2cce4
Sean Svihla <spsvihla@gmail.com>
2024-10-12 21:46:37 -0600

    Add comments to .env_setup.sh

--- .env_setup.sh
+++ .env_setup.sh
@@ -1,7 +1,10 @@
+# set up vscode environment
 python3 -m venv .venv 
 source .venv/bin/activate
 pip install numpy
 pip install .
 echo "PYTHONPATH=.venv/lib/python3.10/site-packages/" > .env
+
+# required to run genomics_example.ipynb
 pip install ipykernel scipy matplotlib
 python3 -m ipykernel install --user --name=.venv --display-name "Python (.venv)"
